d2531b93d521fd59d23a09fea4a950709265491b
Rewath <9987781+rewathkafley@users.noreply.github.com>
2019-11-10 15:09:55 +0600

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if (process.env.PORT) {
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Welcome to IBM Cloud DevOps using containers. Let\'s go use the Continuous Delivery Service');
+  res.send('Test NodeJS APp);
 });
 
 app.listen(PORT);
